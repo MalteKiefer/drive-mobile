b47f2baea274a7070c98cd8879287effd323d0d0
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-08-09 00:22:19 +0200

    Settings menu, first draft

--- src/screens/Configuration/index.tsx
+++ src/screens/Configuration/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableHighlight, View } from 'react-native';
 import { connect } from 'react-redux';
+import AppMenu from '../../components/AppMenu';
+import { Reducers } from '../../redux/reducers/reducers';
+import * as Unicons from '@iconscout/react-native-unicons'
+import { tailwind } from '../../helpers/designSystem';
 
-function Configuration(): JSX.Element {
+function ConfigurationItem(props: {
+  title: string
+}) {
+  return <TouchableHighlight>
+    <View style={[tailwind('bg-white flex-row'), {
+      height: 56,
+      borderBottomWidth: 1,
+      borderColor: '#DFE1E6',
+      padding: 17
+    }]}>
+      <View style={tailwind('flex-grow justify-center')}>
+        <Text>{props.title}</Text>
+      </View>
+      <View style={tailwind('justify-center')}>
+        <Unicons.UilAngleRightB color="#C1C7D0" />
+      </View>
+    </View>
+  </TouchableHighlight>
+}
+
+function ConfigurationGap() {
+  return <View style={{ height: 17 }} />
+}
+
+function Configuration(props: Reducers): JSX.Element {
   return <View>
-    <Text>Settings Screen</Text>
+    <AppMenu {...props} title="Settings" hideSearch={true} hideOptions={true} />
+    <ConfigurationItem title="Storage" />
+    <ConfigurationItem title="Billing" />
+    <ConfigurationGap />
+    <ConfigurationItem title="Security" />
+    <ConfigurationItem title="Change password" />
+    <ConfigurationGap />
+    <ConfigurationItem title="Contact" />
+    <ConfigurationItem title="More info" />
+    <ConfigurationItem title="Log out" />
   </View>
 }
 const mapStateToProps = (state: any) => {
