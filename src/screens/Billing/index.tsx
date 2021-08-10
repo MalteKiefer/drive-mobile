73bb831391b32d83f2c1732ff0a713e567e72dbb
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-08-10 20:22:20 +0200

    Billing accordion, first draft

--- src/screens/Billing/index.tsx
+++ src/screens/Billing/index.tsx
@@ -4,19 +4,29 @@ import { View } from 'react-native';
 import { connect } from 'react-redux';
 import AppMenu from '../../components/AppMenu';
 import { Reducers } from '../../redux/reducers/reducers';
+import BusinessTab from './BusinessTab';
 import IndividualsTab from './IndividualsTab';
 
 const Tab = createMaterialTopTabNavigator();
 
+function BillingTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Individuals" component={IndividualsTab} />
+      <Tab.Screen name="Business" component={BusinessTab} />
+    </Tab.Navigator>
+  );
+}
+
 function Billing(props: Reducers) {
-  return <View>
+  return <View style={{ flex: 1, backgroundColor: 'white' }}>
     <AppMenu {...props} title="Billing" onBackPress={() => {
       props.navigation.goBack();
     }} />
 
-    <Tab.Navigator>
-      <Tab.Screen name="Individuals" component={IndividualsTab} />
-    </Tab.Navigator>
+    <View style={{ flex: 1 }}>
+      <BillingTabs />
+    </View>
 
   </View>;
 }
